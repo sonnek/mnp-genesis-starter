90c63794eaca6b5c3ef4afa0ba28448f1a3114f1
rachelle <rachelle@milesnorthproductions.com>
2018-05-23 22:53:19 -0500

    set up Modular CSS and  basic typography

--- styles/typography/Sassy-Gridlover/test/travis.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-result = `sass example/scss/main.scss example/css/main.css`
-raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('example/css/main.css')
-puts "Regular compile worked successfully"
